2f453d91a74faade6d556f2ec0e508f463ca1571
36115152016.2 <36115152016.2@36DE194600.senacrjedu.local>
2017-09-01 11:58:57 -0300

    Verificar o projeto

---
+++ src/main/java/control/ServletProduto.java
@@ -0,0 +1,41 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletProduto
+ */
+@WebServlet({ "/prd/cadastrar", "/prd/editar", "/prd/listar", "/prd/excluir", "/prd/buscar" })
+public class ServletProduto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletProduto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/entity/Produto.java
@@ -0,0 +1,80 @@
+package entity;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String nmProduto;
+	private String descProduto;
+	private double preco;
+	private int quantidade;
+	
+	public Produto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNmProduto() {
+		return nmProduto;
+	}
+
+	public void setNmProduto(String nmProduto) {
+		this.nmProduto = nmProduto;
+	}
+
+	public String getDescProduto() {
+		return descProduto;
+	}
+
+	public void setDescProduto(String descProduto) {
+		this.descProduto = descProduto;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
